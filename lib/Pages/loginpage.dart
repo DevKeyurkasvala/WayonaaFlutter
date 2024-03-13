49c7ea65dc727aec34a60a3343b451db8ce1ca82
KEYUR KASVALA <devkeyurkasvala@gmail.com>
2024-03-13 15:14:37 +0530

    Login worked with Shared prefrences

--- lib/Pages/loginpage.dart
+++ lib/Pages/loginpage.dart
@@ -1,14 +1,91 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
-class MyLoginPage extends StatelessWidget {
+class MyLoginPage extends StatefulWidget {
   const MyLoginPage({Key? key}) : super(key: key);
 
   @override
+  _MyLoginPageState createState() => _MyLoginPageState();
+}
+
+class _MyLoginPageState extends State<MyLoginPage> {
+  final TextEditingController _userIdController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+    Future<void> _checkLoggedIn() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String? userData = prefs.getString('user');
+
+    if (userData != null) {
+      // User is already logged in, navigate to dashboard
+      Navigator.pushReplacementNamed(context, '/dashboard');
+    }
+  }
+
+    @override
+  void initState() {
+    super.initState();
+    _checkLoggedIn();
+  }
+
+
+  Future<void> _login() async {
+    final String identifier = _userIdController.text;
+    final String password = _passwordController.text;
+
+    final Uri loginUri = Uri.parse('http://125.22.173.98:8000/api/users/login');
+
+    try {
+      final http.Response response = await http.post(
+        loginUri,
+        body: jsonEncode({'identifier': identifier, 'password': password}),
+        headers: {'Content-Type': 'application/json'},
+      );
+
+      if (response.statusCode == 200) {
+        // Login successful, save user information and navigate to dashboard
+        final Map<String, dynamic> responseData = jsonDecode(response.body);
+        final String message = responseData['message'];
+        final dynamic user = responseData['user'];
+
+        // Save user information in shared preferences
+        final SharedPreferences prefs = await SharedPreferences.getInstance();
+        await prefs.setString('user', jsonEncode(user));
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(message)),
+        );
+
+        // Navigate to dashboard page
+        Navigator.pushReplacementNamed(context, '/dashboard');
+      } else if (response.statusCode == 401 || response.statusCode == 404) {
+        final Map<String, dynamic> responseData = jsonDecode(response.body);
+        final String errorMessage = responseData['message'];
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(errorMessage)),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Failed to log in. Please try again later.')),
+        );
+      }
+    } catch (error) {
+      print('Error logging in: $error');
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Error logging in. Please try again later.')),
+      );
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    Color buttonColor = const Color(0xFFFB7C00);
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
+      backgroundColor: Colors.white,
       body: Padding(
         padding: const EdgeInsets.all(12.0),
         child: Column(
@@ -16,34 +93,50 @@ class MyLoginPage extends StatelessWidget {
           children: [
             // Logo
             Image.asset(
-              'assets/primary_logo.png', // Adjust the path according to your logo image asset
+              'assets/primary_logo.png',
               width: 200,
               height: 200,
             ),
             const SizedBox(height: 10),
             // Username TextField
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Username',
+            TextField(
+              controller: _userIdController,
+              decoration: const InputDecoration(
+                labelText: 'User ID',
                 border: OutlineInputBorder(),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey),
+                ),
               ),
             ),
             const SizedBox(height: 20),
             // Password TextField
-            const TextField(
-              decoration: InputDecoration(
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
                 labelText: 'Password',
                 border: OutlineInputBorder(),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey),
+                ),
               ),
               obscureText: true,
             ),
             const SizedBox(height: 20),
             // Login Button
-            ElevatedButton(
-              onPressed: () {
-                // Add login functionality here
-              },
-              child: const Text('Login'),
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: _login,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: buttonColor,
+                ),
+                child: const Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
             ),
           ],
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:wayonaacrm/Pages/dashboard.dart';
+import 'package:wayonaacrm/Pages/loginpage.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SharedPreferences.getInstance();
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // Replace this with your actual authentication logic
+    bool isLoggedIn = false; // Set to true if the user is logged in
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.deepPurple),
-        primarySwatch: Colors.deepPurple,
-        brightness: Brightness.light,
-      ),
-      home: const MyHomePage(),
-    );
+  routes: {
+    '/': (context) => const MyLoginPage(), // Define the route for the root of your app
+    '/dashboard': (context) => const MyHomePage(), // Define the route for the dashboard
+  },
+);
+
   }
 }
-
-
-
